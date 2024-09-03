7e4cc39387a028297ebfb6cbd9618048b2489644
VitokoMP <victor19.mp@gmail.com>
2024-09-03 18:13:44 -0400

    configuracion del webpack en el mode: 'development'

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,25 @@
+const path = require('path');
+
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: __dirname,
-        filename: "./dist/bundle.js"
-    },
-    module: {
-        loaders: [
-        {
-            test: /.js$/,
-            loader: "babel-loader",
-            exclude: /node_modules/,
-            options: {
-            presets: [["env", "react"]],
-            plugins: ["transform-class-properties"]
-            }
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
         }
-        ]
-    }
+      }
+    ]
+  },
+  mode: 'development'
 };
+    
