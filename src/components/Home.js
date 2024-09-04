ce83582aad7911225b9cc20ea54fb5af026c4c33
VitokoMP <victor19.mp@gmail.com>
2024-09-04 02:56:26 -0400

    add HOME, now testing!

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+// components/Home.js
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Hello World, im at home!</h1>
+            {/* Aquí va el contenido de tu página de inicio */}
+        </div>
+    );
+}
+
+export default Home;
